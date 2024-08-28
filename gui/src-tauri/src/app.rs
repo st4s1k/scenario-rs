c3bf5b977b3569703290aea95f15b1eab067a731
Stanislav <st4s1k24@gmail.com>
2024-08-29 02:10:08 +0300

    remove dbg macro calls

--- gui/src-tauri/src/app.rs
+++ gui/src-tauri/src/app.rs
@@ -41,18 +41,14 @@ impl ScenarioAppState {
     }
 
     pub fn load_state(&mut self) {
-        dbg!("Loading state");
         if let Ok(json) = std::fs::read_to_string(Self::STATE_FILE_PATH) {
-            dbg!(json.clone());
             if let Ok(loaded_state) = serde_json::from_str::<ScenarioAppStateConfig>(&json) {
-                dbg!(loaded_state.clone());
                 let config_path = loaded_state.config_path;
                 self.config_path = config_path.clone();
                 self.output_log = loaded_state.output_log;
                 self.load_config(config_path.as_str());
             }
         }
-        dbg!("Finished loading state");
     }
 
     pub fn save_state(&mut self) {
@@ -68,18 +64,13 @@ impl ScenarioAppState {
     }
 
     pub fn load_config(&mut self, config_path: &str) {
-        dbg!("Loading scenario config");
-        dbg!(config_path);
-
         let Ok(config_path) = PathBuf::from_str(config_path) else {
-            dbg!("Invalid config path");
             self.log_message(format!("{SEPARATOR}\nInvalid config path\n{SEPARATOR}\n"));
             return;
         };
 
         match ScenarioConfig::try_from(config_path) {
             Ok(config) => {
-                dbg!("Scenario config loaded");
                 self.log_message(format!(
                     "{SEPARATOR}\nScenario config loaded\n{SEPARATOR}\n"
                 ));
@@ -87,22 +78,16 @@ impl ScenarioAppState {
                 self.config = Some(config);
             }
             Err(e) => {
-                dbg!("Failed to load scenario config");
                 self.log_message(format!(
                     "{SEPARATOR}\nFailed to load scenario config: {e}\n{SEPARATOR}\n"
                 ));
                 let _ = self.app_handle.emit_all("log-update", ());
             }
         }
-
-        dbg!("Finished loading scenario config");
     }
 
     pub fn execute_scenario(&mut self) {
-        dbg!("Executing scenario");
-
         let Some(config) = &self.config else {
-            dbg!("No scenario config file loaded");
             self.log_message(format!(
                 "{SEPARATOR}\nNo scenario config file loaded\n{SEPARATOR}\n"
             ));
@@ -114,12 +99,10 @@ impl ScenarioAppState {
 
         let scenario = match Scenario::new(config.clone()) {
             Ok(scenario) => {
-                dbg!("Scenario loaded");
                 self.log_message(format!("{SEPARATOR}\nScenario loaded\n{SEPARATOR}\n"));
                 scenario
             }
             Err(e) => {
-                dbg!("Failed to load scenario");
                 self.log_message(format!(
                     "{SEPARATOR}\nFailed to load scenario: {e}\n{SEPARATOR}\n"
                 ));
@@ -130,20 +113,13 @@ impl ScenarioAppState {
         self.is_executing = true;
 
         match scenario.execute_with_lifecycle(lifecycle_handler) {
-            Ok(_) => {
-                dbg!("Scenario completed successfully");
-                self.log_message(format!(
-                    "{SEPARATOR}\nScenario completed successfully!\n{SEPARATOR}\n"
-                ))
-            }
-            Err(e) => {
-                dbg!("Scenario failed");
-                self.log_message(format!("{SEPARATOR}\nScenario failed: {e}\n{SEPARATOR}\n"));
-            }
+            Ok(_) => self.log_message(format!(
+                "{SEPARATOR}\nScenario completed successfully!\n{SEPARATOR}\n"
+            )),
+            Err(e) => self.log_message(format!("{SEPARATOR}\nScenario failed: {e}\n{SEPARATOR}\n")),
         }
 
         self.is_executing = false;
-        dbg!("Finished executing scenario");
     }
 
     fn log_message(&mut self, message: String) {
