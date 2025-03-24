e9678f0e2c022e8e490d40befd86ea5e28656ab5
Stanislav <st4s1k24@gmail.com>
2025-03-24 04:57:56 +0200

    test: add unit tests for RemoteSudo execution handling and error cases

--- core/src/scenario/remote_sudo.rs
+++ core/src/scenario/remote_sudo.rs
@@ -82,3 +82,359 @@ impl RemoteSudo {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        scenario::variables::Variables,
+        session::{Channel, SessionType, Sftp},
+    };
+    use std::{
+        panic,
+        sync::{mpsc, Arc, Mutex},
+    };
+
+    struct TestWrite;
+    impl crate::session::Write for TestWrite {
+        fn write_all(&mut self, _buf: &[u8]) -> Result<(), ssh2::Error> {
+            Ok(())
+        }
+    }
+
+    struct TestSftp;
+    impl Sftp for TestSftp {
+        fn create(
+            &self,
+            _path: &std::path::Path,
+        ) -> Result<Box<dyn crate::session::Write>, ssh2::Error> {
+            Ok(Box::new(TestWrite))
+        }
+    }
+
+    struct TestChannel;
+    impl Channel for TestChannel {
+        fn exec(&mut self, _command: &str) -> Result<(), ssh2::Error> {
+            Ok(())
+        }
+        fn read_to_string(&mut self, _output: &mut String) -> Result<usize, ssh2::Error> {
+            Ok(0)
+        }
+        fn exit_status(&self) -> Result<i32, ssh2::Error> {
+            Ok(0)
+        }
+    }
+
+    #[test]
+    fn test_getters() {
+        // Given
+        let remote_sudo = RemoteSudo {
+            command: "echo test".into(),
+        };
+
+        // When & Then
+        assert_eq!(remote_sudo.command(), "echo test");
+    }
+
+    #[test]
+    fn test_execute_success() {
+        // Given
+        struct SuccessChannel;
+        impl Channel for SuccessChannel {
+            fn exec(&mut self, _command: &str) -> Result<(), ssh2::Error> {
+                Ok(())
+            }
+            fn read_to_string(&mut self, output: &mut String) -> Result<usize, ssh2::Error> {
+                output.push_str("Success output");
+                Ok(14)
+            }
+            fn exit_status(&self) -> Result<i32, ssh2::Error> {
+                Ok(0)
+            }
+        }
+
+        let remote_sudo = RemoteSudo {
+            command: "echo success".into(),
+        };
+        let session = Session {
+            inner: SessionType::Test {
+                channel: Arc::new(Mutex::new(SuccessChannel)),
+                sftp: Arc::new(Mutex::new(TestSftp)),
+            },
+        };
+        let variables = Variables::default();
+        let (tx, rx) = mpsc::channel();
+
+        // When
+        let result = remote_sudo.execute(&session, &variables, &tx);
+
+        // Then
+        assert!(result.is_ok());
+
+        let events: Vec<Event> = rx.try_iter().collect();
+        assert_eq!(events.len(), 2);
+        assert!(events
+            .iter()
+            .any(|e| matches!(e, Event::RemoteSudoBefore(_))));
+        assert!(events
+            .iter()
+            .any(|e| matches!(e, Event::RemoteSudoChannelOutput(_))));
+    }
+
+    #[test]
+    fn test_command_placeholder_resolution_error() {
+        // Given
+        let remote_sudo = RemoteSudo {
+            command: "{{ missing_var }}".into(),
+        };
+        let session = Session {
+            inner: SessionType::Test {
+                channel: Arc::new(Mutex::new(TestChannel)),
+                sftp: Arc::new(Mutex::new(TestSftp)),
+            },
+        };
+        let variables = Variables::default();
+        let (tx, rx) = mpsc::channel();
+
+        // When
+        let result = remote_sudo.execute(&session, &variables, &tx);
+
+        // Then
+        assert!(matches!(
+            result,
+            Err(RemoteSudoError::CannotResolveCommandPlaceholders(_))
+        ));
+
+        let events: Vec<Event> = rx.try_iter().collect();
+        assert!(events.is_empty());
+    }
+
+    #[test]
+    fn test_execute_channel_exec_failure() {
+        // Given
+        struct ExecFailChannel;
+        impl Channel for ExecFailChannel {
+            fn exec(&mut self, _command: &str) -> Result<(), ssh2::Error> {
+                Err(ssh2::Error::from_errno(ssh2::ErrorCode::Session(libc::EIO)))
+            }
+            fn read_to_string(&mut self, _output: &mut String) -> Result<usize, ssh2::Error> {
+                Ok(0)
+            }
+            fn exit_status(&self) -> Result<i32, ssh2::Error> {
+                Ok(0)
+            }
+        }
+
+        let remote_sudo = RemoteSudo {
+            command: "test".into(),
+        };
+        let session = Session {
+            inner: SessionType::Test {
+                channel: Arc::new(Mutex::new(ExecFailChannel)),
+                sftp: Arc::new(Mutex::new(TestSftp)),
+            },
+        };
+        let variables = Variables::default();
+        let (tx, rx) = mpsc::channel();
+
+        // When
+        let result = remote_sudo.execute(&session, &variables, &tx);
+
+        // Then
+        assert!(matches!(
+            result,
+            Err(RemoteSudoError::CannotExecuteRemoteCommand(_))
+        ));
+
+        let events: Vec<Event> = rx.try_iter().collect();
+        assert_eq!(events.len(), 1);
+        assert!(events
+            .iter()
+            .any(|e| matches!(e, Event::RemoteSudoBefore(_))));
+    }
+
+    #[test]
+    fn test_execute_channel_nonzero_exit_status() {
+        // Given
+        struct NonZeroExitChannel;
+        impl Channel for NonZeroExitChannel {
+            fn exec(&mut self, _command: &str) -> Result<(), ssh2::Error> {
+                Ok(())
+            }
+            fn read_to_string(&mut self, output: &mut String) -> Result<usize, ssh2::Error> {
+                output.push_str("error output");
+                Ok(12)
+            }
+            fn exit_status(&self) -> Result<i32, ssh2::Error> {
+                Ok(1)
+            }
+        }
+
+        let remote_sudo = RemoteSudo {
+            command: "test".into(),
+        };
+        let session = Session {
+            inner: SessionType::Test {
+                channel: Arc::new(Mutex::new(NonZeroExitChannel)),
+                sftp: Arc::new(Mutex::new(TestSftp)),
+            },
+        };
+        let variables = Variables::default();
+        let (tx, rx) = mpsc::channel();
+
+        // When
+        let result = remote_sudo.execute(&session, &variables, &tx);
+
+        // Then
+        assert!(matches!(
+            result,
+            Err(RemoteSudoError::RemoteCommandFailedWithStatusCode(1))
+        ));
+
+        let events: Vec<Event> = rx.try_iter().collect();
+        assert_eq!(events.len(), 2);
+        assert!(events
+            .iter()
+            .any(|e| matches!(e, Event::RemoteSudoBefore(_))));
+        assert!(events
+            .iter()
+            .any(|e| matches!(e, Event::RemoteSudoChannelOutput(_))));
+    }
+
+    #[test]
+    fn test_execute_read_output_failure() {
+        // Given
+        struct ReadFailChannel;
+        impl Channel for ReadFailChannel {
+            fn exec(&mut self, _command: &str) -> Result<(), ssh2::Error> {
+                Ok(())
+            }
+            fn read_to_string(&mut self, _output: &mut String) -> Result<usize, ssh2::Error> {
+                Err(ssh2::Error::from_errno(ssh2::ErrorCode::Session(libc::EIO)))
+            }
+            fn exit_status(&self) -> Result<i32, ssh2::Error> {
+                Ok(0)
+            }
+        }
+
+        let remote_sudo = RemoteSudo {
+            command: "test".into(),
+        };
+        let session = Session {
+            inner: SessionType::Test {
+                channel: Arc::new(Mutex::new(ReadFailChannel)),
+                sftp: Arc::new(Mutex::new(TestSftp)),
+            },
+        };
+        let variables = Variables::default();
+        let (tx, rx) = mpsc::channel();
+
+        // When
+        let result = remote_sudo.execute(&session, &variables, &tx);
+
+        // Then
+        assert!(matches!(
+            result,
+            Err(RemoteSudoError::CannotReadChannelOutput(_))
+        ));
+
+        let events: Vec<Event> = rx.try_iter().collect();
+        assert_eq!(events.len(), 1);
+        assert!(events
+            .iter()
+            .any(|e| matches!(e, Event::RemoteSudoBefore(_))));
+    }
+
+    #[test]
+    fn test_execute_exit_status_failure() {
+        // Given
+        struct ExitStatusFailChannel;
+        impl Channel for ExitStatusFailChannel {
+            fn exec(&mut self, _command: &str) -> Result<(), ssh2::Error> {
+                Ok(())
+            }
+            fn read_to_string(&mut self, _output: &mut String) -> Result<usize, ssh2::Error> {
+                Ok(0)
+            }
+            fn exit_status(&self) -> Result<i32, ssh2::Error> {
+                Err(ssh2::Error::from_errno(ssh2::ErrorCode::Session(libc::EIO)))
+            }
+        }
+
+        let remote_sudo = RemoteSudo {
+            command: "test".into(),
+        };
+        let session = Session {
+            inner: SessionType::Test {
+                channel: Arc::new(Mutex::new(ExitStatusFailChannel)),
+                sftp: Arc::new(Mutex::new(TestSftp)),
+            },
+        };
+        let variables = Variables::default();
+        let (tx, rx) = mpsc::channel();
+
+        // When
+        let result = remote_sudo.execute(&session, &variables, &tx);
+
+        // Then
+        assert!(matches!(
+            result,
+            Err(RemoteSudoError::CannotObtainRemoteCommandExitStatus(_))
+        ));
+
+        let events: Vec<Event> = rx.try_iter().collect();
+        assert_eq!(events.len(), 2);
+        assert!(events
+            .iter()
+            .any(|e| matches!(e, Event::RemoteSudoBefore(_))));
+        assert!(events
+            .iter()
+            .any(|e| matches!(e, Event::RemoteSudoChannelOutput(_))));
+    }
+
+    #[test]
+    fn test_channel_lock_error() {
+        // Given
+        let remote_sudo = RemoteSudo {
+            command: "test".into(),
+        };
+
+        let channel_mutex: Arc<Mutex<TestChannel>> = Arc::new(Mutex::new(TestChannel));
+        let channel_mutex_clone = Arc::clone(&channel_mutex);
+        let _ = std::thread::spawn(move || {
+            panic::set_hook(Box::new(|_info| {
+                // do nothing
+            }));
+            let _ = panic::catch_unwind(|| {
+                let _guard = channel_mutex_clone.lock().unwrap();
+                panic!("Deliberately poisoning the mutex");
+            });
+        })
+        .join();
+
+        let session = Session {
+            inner: SessionType::Test {
+                channel: channel_mutex,
+                sftp: Arc::new(Mutex::new(TestSftp)),
+            },
+        };
+
+        let variables = Variables::default();
+        let (tx, rx) = mpsc::channel();
+
+        // When
+        let result = remote_sudo.execute(&session, &variables, &tx);
+
+        // Then
+        assert!(matches!(
+            result,
+            Err(RemoteSudoError::CannotGetALockOnChannel)
+        ));
+
+        let events: Vec<Event> = rx.try_iter().collect();
+        assert_eq!(events.len(), 1);
+        assert!(events
+            .iter()
+            .any(|e| matches!(e, Event::RemoteSudoBefore(_))));
+    }
+}
