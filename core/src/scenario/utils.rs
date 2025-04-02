ee4242fb2f251d1443fe7055646dd3c0e0e8af5f
Stanislav <st4s1k24@gmail.com>
2025-04-02 20:19:24 +0300

    refactor: generalize SendEvent trait for improved flexibility and error handling

--- core/src/scenario/utils.rs
+++ core/src/scenario/utils.rs
@@ -1,8 +1,22 @@
+use regex::Regex;
 use std::sync::mpsc::Sender;
 
-use regex::Regex;
+pub trait SendEvent<T> {
+    fn send_event(&self, event: T);
+}
 
-use super::events::ScenarioEvent;
+impl<T: Clone + std::fmt::Debug> SendEvent<T> for Sender<T> {
+    fn send_event(&self, event: T) {
+        if let Err(_) = self.send(event.clone()) {
+            eprintln!(
+                "Warning: Failed to send {} event (channel closed)",
+                std::any::type_name::<T>()
+            );
+            #[cfg(debug_assertions)]
+            eprintln!("  Event details: {:?}", event);
+        }
+    }
+}
 
 pub(crate) trait HasPlaceholders
 where
@@ -10,24 +24,11 @@ where
 {
     fn has_placeholders(&self) -> bool {
         let placeholder_regex =
-            Regex::new(r"\{[^}]+\}").expect("`placeholder_regex` should be a valid regex");
-        let value = self.as_ref();
-        placeholder_regex.find(value).is_some()
+            Regex::new(r"\{[^}]+\}").expect("placeholder_regex should be a valid regex");
+
+        placeholder_regex.find(self.as_ref()).is_some()
     }
 }
 
 impl HasPlaceholders for String {}
 impl HasPlaceholders for &str {}
-
-pub (crate) trait SendEvent<T> {
-    fn send_event(&self, event: T);
-}
-
-impl SendEvent<ScenarioEvent> for Sender<ScenarioEvent> {
-    fn send_event(&self, event: ScenarioEvent) {
-        if self.send(event.clone()).is_err() {
-            eprintln!("Warning: Could not send event, channel may be closed");
-            dbg!(event);
-        }
-    }
-}
