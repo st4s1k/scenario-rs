ffaea1c0455a49c1242a0f587c5058453b88c1f1
Stanislav <st4s1k24@gmail.com>
2025-04-13 01:48:17 +0300

    refactor: rename scr\trace\layers\scenario.rs to scr\trace\layers\scenario_layer.rs

--- gui/src-tauri/src/trace/layers/mod.rs
+++ gui/src-tauri/src/trace/layers/mod.rs
@@ -1,9 +1,9 @@
 pub use app_layer::AppEventLayer;
-pub use scenario::ScenarioEventLayer;
+pub use scenario_layer::ScenarioEventLayer;
 use tracing::Event;
 
 mod app_layer;
-mod scenario;
+mod scenario_layer;
 
 pub trait EventLayer {
     fn process_event(&self, event: &Event<'_>);
