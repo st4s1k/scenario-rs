3f014137d3b13753c026386621f4100f8e265ec5
Stanislav <st4s1k24@gmail.com>
2025-05-05 00:37:45 +0300

    feat: replace tooltip spans with tooltip component in titlebar

--- gui/src/app/titlebar/titlebar.component.ts
+++ gui/src/app/titlebar/titlebar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { invoke } from "@tauri-apps/api/core";
 import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow'
+import { TooltipComponent } from '../shared/tooltip/tooltip.component';
 const appWindow = getCurrentWebviewWindow()
 
 @Component({
   selector: 'titlebar',
-  imports: [],
+  imports: [
+    TooltipComponent
+  ],
   templateUrl: './titlebar.component.html',
   styleUrl: './titlebar.component.scss'
 })
