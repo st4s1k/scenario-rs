2f2e0ef1cee5f9dd429fef8818da7e6b0c2a6889
Stanislav <st4s1k24@gmail.com>
2025-05-11 00:52:13 +0300

    feat: refactor expandable component to remove variant input and integrate size enum

---
+++ gui/src/app/models/enums.ts
@@ -0,0 +1,5 @@
+
+export type ComponentSize =
+    'small'
+    | 'medium'
+    | 'large';
--- gui/src/app/shared/expandable/expandable.component.ts
+++ gui/src/app/shared/expandable/expandable.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, Output, EventEmitter, HostBinding } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ComponentSize } from '../../models/enums';
+
+export type ExpandableSeparatorVariant = 'line' | 'gap';
 
 @Component({
   selector: 'expandable',
@@ -8,13 +11,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './expandable.component.scss'
 })
 export class ExpandableComponent {
-  @Input() variant: 'primary' | 'error' = 'primary';
+
   @Input() expanded: boolean = true;
   @Input() showSeparator: boolean = false;
-  @Input() separatorVariant: 'line' | 'gap' = 'line';
-  @Input() separatorSize: 'small' | 'medium' | 'large' = 'medium';
+  @Input() separatorVariant: ExpandableSeparatorVariant = 'line';
+  @Input() separatorSize: ComponentSize = 'medium';
   @Input() nested: boolean = false;
+
   @Output() expandedChange = new EventEmitter<boolean>();
+
   @HostBinding('class.nested') get isNested() { return this.nested; }
   @HostBinding('class.expanded') get isExpanded() { return this.expanded; }
 
